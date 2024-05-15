ee89ee7831e4c73e96f25c39816bb2d9e1ff1c06
rahilaode <laoderahilatansa@gmail.com>
2024-05-15 17:27:50 +0700

    fix comment

--- main.tf
+++ main.tf
@@ -1,29 +1,32 @@
+# Define required providers, in this case, AWS. 
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.0"  # Constraint on the AWS provider version
     }
   }
 }
 
+# Configure the AWS provider with the specified region, access key, and secret key.
 provider "aws" {
   region     = var.AWS_REGION
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
 
-# Creating key-pair on AWS using SSH-public key
+# Create an SSH key pair resource in AWS using a provided public key file.
 resource "aws_key_pair" "deployer" {
   key_name   = var.KEY-NAME
   public_key = file("./demo-devops-class.pub")
 }
 
-# Creating a security group to restrict/allow inbound connectivity
+# Create a security group to control inbound traffic to instances.
 resource "aws_security_group" "network-security-group" {
   name        = var.SECURITY-GROUP-NAME
   description = "Allow TLS inbound traffic"
 
+  # Allow SSH inbound traffic from any IP address.
   ingress {
     description = "SSH"
     from_port   = 22
@@ -31,31 +34,33 @@ resource "aws_security_group" "network-security-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # Not recommended to add "0.0.0.0/0" instead we need to be more specific with the IP ranges to allow connectivity from.
+  # Add tags for better identification.
   tags = {
     Name = "SG-demo-devops-class"
   }
 }
 
-# Create ubuntu vm for productions
+# Create an AWS instance for production environment.
 resource "aws_instance" "demo-devops-prd" {
-  ami                    = "ami-00c31062c5966e820"
-  instance_type          = "t3.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.network-security-group.id]
+  ami                    = "ami-00c31062c5966e820"  # Ubuntu AMI
+  instance_type          = "t3.micro"  # Instance type
+  key_name               = aws_key_pair.deployer.key_name  # SSH key pair for authentication
+  vpc_security_group_ids = [aws_security_group.network-security-group.id]  # Attach the security group
 
+  # Add tags for better identification.
   tags = {
     Name = "demo-devops-prd"
   }
 }
 
-# Create ubuntu vm for staging
+# Create an AWS instance for staging environment.
 resource "aws_instance" "demo-devops-stg" {
-  ami                    = "ami-00c31062c5966e820"
-  instance_type          = "t3.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.network-security-group.id]
+  ami                    = "ami-00c31062c5966e820"  # Ubuntu AMI
+  instance_type          = "t3.micro"  # Instance type
+  key_name               = aws_key_pair.deployer.key_name  # SSH key pair for authentication
+  vpc_security_group_ids = [aws_security_group.network-security-group.id]  # Attach the security group
 
+  # Add tags for better identification.
   tags = {
     Name = "demo-devops-stg"
   }
